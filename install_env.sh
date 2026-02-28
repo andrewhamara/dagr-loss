bbe6000846f23ba33197853fc6696e2e063c4f11
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2026-02-28 15:45:45 -0600

    resolve dependency issues in environment

--- install_env.sh
+++ install_env.sh
@@ -4,9 +4,11 @@ TORCH=$(python -c "import torch; print(torch.__version__)")
 CUDA=$(python -c "import torch; print(torch.version.cuda)")
 URL=https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
 
-pip install --no-cache-dir torch-scatter -f $URL;
-pip install --no-cache-dir torch-cluster -f $URL;
+pip install torch-scatter torch-cluster \
+	  -f https://data.pyg.org/whl/torch-1.11.0+cu113.html
 pip install --no-cache-dir torch-spline-conv -f $URL;
-pip install --no-cache-dir torch-sparse -f $URL;
-pip install torch-geometric;
+pip install torch-sparse==0.6.13 \
+	  -f https://data.pyg.org/whl/torch-1.11.0+cu113.html;
+pip install torch-geometric==2.0.4
 pip install wandb numba hdf5plugin plotly matplotlib pycocotools opencv-python scikit-video pandas ruamel.yaml
+pip install "Pillow<10.0.0"
